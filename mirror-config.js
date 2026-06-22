f25feef16034d899a943b1aacf0ab03f6c2088c0
Dimitri Le Roux <dimitri@synarchy.pro>
2026-06-22 14:04:48 +0200

    Update mirror-config.js

--- mirror-config.js
+++ mirror-config.js
@@ -3,4 +3,4 @@
  * After deploying the bound Google Apps Script web app, replace the empty string
  * with its /exec URL. Keep this file public: it contains no credentials.
  */
-window.HRM_SUBMISSION_ENDPOINT = "";
+window.HRM_SUBMISSION_ENDPOINT = "https://script.google.com/macros/s/AKfycbyZcZ0H_15_YuAT8YweVIvMaRUPmYxQu8Q4Rq9kJ0oLJIv9DLQNcSqWGqjOGZ-RChUq/exec";
